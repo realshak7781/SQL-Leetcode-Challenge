c7b051ebbc20f9a8a7fe2ae6c4670ddd18fa17ad
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-30 21:58:22 +0530

    Update 602. Friend Requests II: Who Has the Most Friends.sql

--- Medium/602. Friend Requests II: Who Has the Most Friends.sql
+++ Medium/602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,5 +1,21 @@
+-- OPTIMAL:
+
+select id, count(*) as num
+from(
+
+select requester_id as id from RequestAccepted
+
+union all
+ 
+select accepter_id from RequestAccepted
+) as sub
+group by id
+order by num desc
+limit 1;
 
 
+
+-- BRUTE FORCE:
 select id, count as num
 from(
 
