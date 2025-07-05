b70a5cdd7c4b4cbf04e57ba8443639d73ef9f820
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-05 11:59:55 +0530

    Create 607. Sales Person.sql

---
+++ Easy/607. Sales Person.sql
@@ -0,0 +1,10 @@
+select s.name as name
+from SalesPerson s
+where s.name not in
+(
+select sp.name
+from salesperson sp
+left join orders o on sp.sales_id=o.sales_id
+left join company c on o.com_id = c.com_id
+where c.name = 'Red'
+)
