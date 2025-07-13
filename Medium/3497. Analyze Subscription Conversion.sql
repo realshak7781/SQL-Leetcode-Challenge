b058f837dfcdc46720cdee32b4e5571befafd38f
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-13 22:30:35 +0530

    Update 3497. Analyze Subscription Conversion.sql

--- Medium/3497. Analyze Subscription Conversion.sql
+++ Medium/3497. Analyze Subscription Conversion.sql
@@ -1,4 +1,14 @@
+-- USING IF ELSE
+select user_id,
 
+round(avg(if(activity_type = 'free_trial',activity_duration,null)),2) as trial_avg_duration,
+round(avg(if(activity_type = 'paid',activity_duration,null)),2) as paid_avg_duration
+from UserActivity
+group by user_id
+having max(if(activity_type = 'free_trial',activity_date,null))<
+      max(if(activity_type = 'paid',activity_date,null))
+
+order by user_id;
 
 -- using CASE WHEN :
 select user_id,
