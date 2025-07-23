43e24b6e6688dbd79708b07f2c437fa340ca3286
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-23 17:26:10 +0530

    Create Euclid's Distance.sql

---
+++ Medium/Euclid's Distance.sql
@@ -0,0 +1,7 @@
+--https://www.interviewbit.com/problems/euclid-s-distance/
+
+SELECT SQRT(
+    POW(MAX(XCoordinate) - MIN(XCoordinate), 2) + 
+    POW(MAX(YCoordinate) - MIN(YCoordinate), 2)
+) AS A
+FROM HOUSES;
