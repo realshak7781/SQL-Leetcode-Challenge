c2bff8fc5bd57d05622763cc74b6099349958da3
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-11-06 23:54:08 +0530

    Create Type of Triangle.sql

---
+++ Hackerrank/Advanced Select/Type of Triangle.sql
@@ -0,0 +1,10 @@
+
+select
+  case 
+  when (a+b)<=c or (b+c)<=a or (a+c)<=b then 'Not A Triangle'
+  when a=b and b=c and a=c then 'Equilateral'
+  when a=b or b=c or a=c then 'Isosceles'
+  else 'Scalene'
+  end as triangle_type
+  
+from Triangles
