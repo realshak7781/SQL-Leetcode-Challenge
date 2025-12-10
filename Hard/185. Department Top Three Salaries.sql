2b7d4a13ef3606369cba6115dd4e1b0120735da6
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-12-10 16:37:00 +0530

    Update 185. Department Top Three Salaries.sql

--- Hard/185. Department Top Three Salaries.sql
+++ Hard/185. Department Top Three Salaries.sql
@@ -1,3 +1,19 @@
+-- Better Approach Using : Window Functions
+select t.DeptName as Department,
+       t.EmpName as Employee,
+       t.EmpSalary as Salary
+from
+(
+select d.name as DeptName,
+       e.name as EmpName,
+       e.salary as EmpSalary,
+dense_rank() over(partition by d.name order by e.salary desc) as salaryRank
+from Employee e inner join Department d on e.departmentId=d.id
+)t where t.salaryRank <=3;
+
+-- Approach 1 :
+-- using SubQuery
+
 SELECT d.name AS Department,
        e.name AS Employee,
        e.salary AS Salary
