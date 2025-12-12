2b7cb4cb8f2a75674d3abe06c67b19c1834d2fdd
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-12-13 01:46:02 +0530

    Update 511. Game Play Analysis I.sql

--- Easy/511. Game Play Analysis I.sql
+++ Easy/511. Game Play Analysis I.sql
@@ -1,3 +1,16 @@
+-- using window functions
+
+select t.player_id as player_id,
+t.event_date as first_login
+from
+(
+select player_id,event_date,
+      row_number() over(partition by player_id order by event_date asc)  as dateRank
+from Activity
+)t where t.dateRank=1;
+
+-- /approach 1
+
 select a1.player_id,a1.event_date as first_login
 from Activity a1
 where a1.event_date=(
