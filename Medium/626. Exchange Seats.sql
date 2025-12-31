04c07a9a23144a39ab57e04fc151bb7a007aa61d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-12-31 15:35:32 +0530

    Update 626. Exchange Seats.sql

--- Medium/626. Exchange Seats.sql
+++ Medium/626. Exchange Seats.sql
@@ -1,3 +1,38 @@
+-- better approach : Improves readability : Using cte and case when
+with cte1 as (
+    select 
+    case 
+       when s2.id is null then s1.id
+       else s2.id
+    end as id,
+    s1.student as student
+
+    from Seat s1 left join Seat s2 on s2.id=s1.id+1
+    where s1.id%2=1
+),
+cte2 as (
+    select 
+    case 
+       when s2.id is null then s1.id
+       else s2.id
+    end as id,
+    s1.student as student
+
+    from Seat s1 left join Seat s2 on s2.id=s1.id-1
+    where s1.id%2=0
+)
+
+(
+    select * from cte1
+
+union
+
+select * from cte2
+) order by id
+
+
+
+-- approach 1 :
 select t1.id,
 
 ifnull((select t2.student from Seat t2 where t2.id=t1.id+1),t1.student) as student
